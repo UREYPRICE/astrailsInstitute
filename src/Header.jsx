05be937842d2dd42a28944b92165087457464511
Ahmed <uchihawa@gmail.com>
2024-04-28 02:11:31 +0500

    Fix menu iocn and add logo

--- src/Header.jsx
+++ src/Header.jsx
@@ -12,11 +12,15 @@ const Header = () => {
   const containerClass = isMenuOpen ? 'container change' : 'container';
 
   return (
-    <div className={containerClass} onClick={handleButtonClick}>
+    <header>
+<div className={containerClass} onClick={handleButtonClick}>
       <div className="bar1"></div>
       <div className="bar2"></div>
       <div className="bar3"></div>
     </div>
+    <span className='logo'>Astralis Institute</span>
+    </header>
+    
   );
 }
 
