94b930a30f0534fb80807b6911a51fc9b4c3b2df
Ahmed <uchihawa@gmail.com>
2024-05-15 00:35:57 +0500

    Add and Start intro section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './Header'
+import Intro from './Intro'
 
 const App = () => {
 
@@ -9,6 +10,7 @@ const App = () => {
 return (
   <div>
      < Header/>
+     <Intro/>
   </div>
 
  
---
+++ src/Intro.jsx
@@ -0,0 +1,19 @@
+import './Intro.css'
+
+
+const Intro = () => {
+    return (
+ <div className="introDiv">
+
+    <h2>with Astralis Institute Coaching</h2>
+    <h1 >Let Go of Your Fears</h1>
+    <p> Discover the simple 3 Steps that I Discovered to Hack Productivity. It Workds 100%. Wanna transform your life?</p>
+
+
+    </div>
+    );
+
+   
+}
+
+export default Intro;
