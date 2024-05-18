3da0ac268798752b75e46c83eee625672f4c090a
Ahmed <uchihawa@gmail.com>
2024-05-18 18:56:38 +0500

    Add button icon to info page

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -12,9 +12,9 @@ const Intro = () => {
     <div className="buttonDiv">
 
     <a href="http://" target="_blank" rel="noopener noreferrer">
+        <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#e8eaed"><path d="M252-198v-535.33q0-23.34 16-39.67 16-16.33 40-16.33h344q24 0 40 16.33t16 39.67V-198l-228-98-228 98Z"/></svg> 
 
-
-        <span>Get Your Free Guide Now!</span>
+        <span className='buttonText'>Get Your Enrollment Now!</span>
 
     </a>
     </div>
