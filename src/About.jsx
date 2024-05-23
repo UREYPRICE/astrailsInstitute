fdb6ea444ae426a359f7be664bda724aac79be23
Ahmed <uchihawa@gmail.com>
2024-05-24 00:34:46 +0500

    added img and add the css to img for About us section

--- src/About.jsx
+++ src/About.jsx
@@ -1,6 +1,6 @@
 
 import './about.css'
-
+import aboutUsImage from './assets/aboutUsImage.jpg'
 const About = ()=> {
 
 
@@ -10,7 +10,13 @@ return(
 <h2>-About Us</h2>
 
 <h1>I help people to discover their true potential</h1>
+
+
+<img className='aboutImg' src={aboutUsImage} alt="" />
+
 </div>
+
+
 
 
 
