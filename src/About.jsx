388a83f1c4cea0794ad88efd2ef02d9360a8eb3f
Ahmed <uchihawa@gmail.com>
2024-05-26 22:21:30 +0500

    almost done with about us first section

--- src/About.jsx
+++ src/About.jsx
@@ -5,19 +5,32 @@ const About = ()=> {
 
 
 return(
+    <>
 
 <div className="about">
 <h2>-About Us</h2>
 
 <h1>I help people to discover their true potential</h1>
 
-
+<div className="aboutImageDiv">
 <img className='aboutImg' src={aboutUsImage} alt="" />
+
+<div className="aboutImageExp">
+
+<span className='aboutImageExpText'>YEARS OF EXPERIENCE</span>
+<span className='aboutImageExpTextNumber'>12</span>
+
+
+</div>
 
 </div>
 
 
 
+</div>
+
+
+</>
 
 
 )
