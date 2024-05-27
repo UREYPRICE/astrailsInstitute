f08b6f21c981d114774d2b9a624a4adb63a94630
Ahmed <uchihawa@gmail.com>
2024-05-28 00:27:31 +0500

    Add about paragraphs

--- src/About.jsx
+++ src/About.jsx
@@ -25,7 +25,17 @@ return(
 
 </div>
 
+<div className="aboutPara">
 
+<p>
+The plans you refer to will soon be back in our hands. But with the blast shield down, I can’t even see! How am I supposed to fight? I suggest you try it again, Luke. This time, let go your conscious self and act on instinct.
+</p>
+
+<p>
+Don’t be too proud of this technological terror you’ve constructed. The ability to destroy a planet is insignificant next to the power of the Force. I find your lack of faith disturbing. Kid, I’ve flown from one side of this galaxy.
+</p>
+
+</div>
 
 </div>
 
