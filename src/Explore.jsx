02ba5505e3784338ab340bc6c2afd7601d7cfe6c
Ahmed <uchihawa@gmail.com>
2024-06-03 23:50:00 +0500

    Explore section populated

--- src/Explore.jsx
+++ src/Explore.jsx
@@ -3,7 +3,7 @@ import './Explore.css'
 
 const Explore = () => {
     return (
-        <div>
+        <div className='ExploreContainer'>
             <h2>Explore</h2>
         <h1>Services We Provide For Our Clients</h1>
         <p>We can Help you in these particular areas.</p>
