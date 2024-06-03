79439e9e2d3429cd42833c584c9b4bd9cc8c7b79
Ahmed <uchihawa@gmail.com>
2024-06-04 01:44:59 +0500

    Added Css to the Explore section still WIP

--- src/Explore.jsx
+++ src/Explore.jsx
@@ -4,6 +4,7 @@ import './Explore.css'
 const Explore = () => {
     return (
         <div className='ExploreContainer'>
+            <div className="ExploreOverlay"></div>
             <h2>Explore</h2>
             <h1>Services We Provide For Our Clients</h1>
             <p>We can Help you in these particular areas.</p>
