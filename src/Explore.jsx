d09af4fe60cf7fbce57209840c9ce899f37d3de2
Ahmed <uchihawa@gmail.com>
2024-06-03 23:50:51 +0500

    Explore section populated

--- src/Explore.jsx
+++ src/Explore.jsx
@@ -7,6 +7,7 @@ const Explore = () => {
             <h2>Explore</h2>
         <h1>Services We Provide For Our Clients</h1>
         <p>We can Help you in these particular areas.</p>
+        <section>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur placeat blanditiis provident quibusdam natus, deserunt eveniet et saepe autem numquam officiis sunt dolores quam nostrum quae assumenda consectetur ut non.</section>
         </div>
         
     )
