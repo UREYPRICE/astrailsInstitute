06a28c04423b90d4380c787bac5fa5328ca3e5e9
Ahmed <uchihawa@gmail.com>
2024-06-06 23:47:53 +0500

    Add card structure to the explore comp

--- src/Explore.jsx
+++ src/Explore.jsx
@@ -8,6 +8,19 @@ const Explore = () => {
             <h2>Explore</h2>
             <h1>Services We Provide For Our Clients</h1>
             <p>We can Help you in these particular areas.</p>
+
+<div className="cardsContainer">
+
+<div className="card">
+
+    
+</div>
+
+
+</div>
+
+
+
         </div>
 
     )
