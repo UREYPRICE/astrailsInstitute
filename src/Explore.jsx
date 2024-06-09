57df27b2c63c2303014593f20addd024554dd281
Ahmed <uchihawa@gmail.com>
2024-06-09 16:17:59 +0500

    Add Card but not finished

--- src/Explore.jsx
+++ src/Explore.jsx
@@ -4,56 +4,56 @@ import './Explore.css'
 const Explore = () => {
     return (
 
-<>
+        <>
 
 
 
 
-        <div className='ExploreContainer'>
-            <div className="ExploreOverlay"></div>
-            <h2>Explore</h2>
-            <h1>Services We Provide For Our Clients</h1>
-            <p>We can Help you in these particular areas.</p>
+            <div className='ExploreContainer'>
+                <div className="ExploreOverlay"></div>
+                <h2>Explore</h2>
+                <h1>Services We Provide For Our Clients</h1>
+                <p>We can Help you in these particular areas.</p>
 
-            <div className = "CardContainerFirst" >
-  <div className="container">
-       <div className="card">
-           <div className="front">
-             
-             <h3>
-    1:1 Coaching
-</h3>
+                <div className="CardContainerFirst" >
+                    <div className="cardContainer">
+                        <div className="card">
+                            <div className="front">
 
-<span>-</span>
-<p> Remember, a Jedi can feel the Force flowing through him. I'm surprised you had the courage to take the responsibility yourself.</p>
+                                <h3>
+                                    1:1 Coaching
+                                </h3>
 
-             
-           </div>
-           <div className="back">
-             
-             <h3>
-    1:1 Coaching
-</h3>
-
-<span>-</span>
-<p> Remember, a Jedi can feel the Force flowing through him. I'm surprised you had the courage to take the responsibility yourself.</p>
-             
-           </div>
-       </div>
-   </div>
+                                <span>-</span>
+                                <p> Remember, a Jedi can feel the Force flowing through him. I'm surprised you had the courage to take the responsibility yourself.</p>
 
 
-</div>
+                            </div>
+                            <div className="back">
+
+                                <h3>
+                                    1:1 Coaching
+                                </h3>
+
+                                <span>-</span>
+                                <p> Remember, a Jedi can feel the Force flowing through him. I'm surprised you had the courage to take the responsibility yourself.</p>
+
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
 
 
 
-        </div>
+            </div>
 
 
-       
 
-       
-</>
+
+
+        </>
 
     )
 }
